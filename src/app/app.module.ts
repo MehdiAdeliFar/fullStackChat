57691359949640c9820b0a6c654eaf18708f2182
mehdi <mehdi_adeli@aut.ac.ir>
2019-03-05 16:01:36 +0330

    remove unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ListComponent} from './list/list.component';
-import {ReserveComponent} from './reserve/reserve.component';
-import {AdminLoginComponent} from './admin/admin-login/admin-login.component';
-import {AdminListComponent} from './admin/admin-list/admin-list.component';
-import {AdminNewVideoComponent} from './admin/admin-new-video/admin-new-video.component';
-import {AdminCustomerListComponent} from './admin/admin-customer-list/admin-customer-list.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
@@ -24,11 +19,7 @@ import { MessageHistoryComponent } from './admin/message-history/message-history
   declarations: [
     AppComponent,
     ListComponent,
-    ReserveComponent,
-    AdminLoginComponent,
-    AdminListComponent,
-    AdminNewVideoComponent,
-    AdminCustomerListComponent,
+
     LoginComponent,
     RegisterComponent,
     ChatsComponent,
